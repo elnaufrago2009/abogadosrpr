5205c61579c0c26a659c38083de30a1896553698
moises <elnaufrago2009@hotmail.com>
2012-10-12 15:34:28 -0500

    init

--- app/controllers/inicios_controller.rb
+++ app/controllers/inicios_controller.rb
@@ -1,15 +1,7 @@
 class IniciosController < ApplicationController
 
 	
-	def index
-
-		@notice = Notice.find(params[:id])
-    	respond_to do |format|
-      		format.html # show.html.erb
-      		format.json { render json: @notice }
-    	end
-
-		
+	def index		
 	end
 
 	def noticias
